d4e1312378ea51ace2e47b4ba54a2f87d1f9e11c
Vijay Kumar <vijaykumar@xeadmins-MacBook-Pro-2.local>
2019-05-23 20:05:03 +0530

    errr

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const tradeRoutes = require('./api/routes/trades'); 
 
+app.use(morgan('dev'));
 app.use('/trades', tradeRoutes)
+
+// error handler
+app.use( (req, res, next) => {
+    const error = new Error("Invalid Routes");
+    error.status = 404;  
+    next(error);
+});
+
+app.use( (error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+
+        }
+    })
+})
+
 module.exports = app;
 
  
